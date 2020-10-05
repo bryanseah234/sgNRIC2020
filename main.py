37155af1aa05a6d1537060888d45222376f973bd
bryanseah234 <bryanseah234@gmail.com>
2020-10-05 19:52:18 +0800

    Update main.py
    added checksum into here, specified templates folder, solved nonetype object

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from copy import copy
 import barcode
 from barcode.writer import ImageWriter
 from barcode.codex import Code39
-from checksum import is_nric_valid
 import os
 import shutil
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates') 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.after_request
@@ -39,6 +38,32 @@ def root():
                 os.remove(path)
     return render_template("index.html", error=error)
 
+def is_nric_valid(nric):
+    nricCheckDigits = 'JZIHGFEDCBA'
+    finCheckDigits = 'XWUTRQPNMLK'
+    weights = [2, 7, 6, 5, 4, 3, 2]
+    if len(nric) != 9:
+        return False
+
+    firstChar = nric[0].upper()
+    digits = nric[1:8]
+    lastChar = nric[8].upper()
+    
+    if firstChar not in "STFG" or not digits.isdigit():
+        return False
+
+    total = 0
+    if firstChar in "GT":
+        total += 4
+
+    for i in range(7):
+        total += int(digits[i]) * weights[i]
+
+    if firstChar in "ST":
+        return nricCheckDigits[total % 11] == lastChar
+
+    return finCheckDigits[total % 11] == lastChar
+
 @app.route('/generate', methods=['GET'])
 def generate():
     if "nric" in request.args:
@@ -50,7 +75,10 @@ def generate():
                 os.remove(path)
         nric = request.args.get('nric')
         print(nric)
-        if is_nric_valid(nric) == True:
+        if nric == '':
+            error = '<Empty Input>'
+            return render_template("index.html", error=error)
+        elif is_nric_valid(nric) == True:
             code39 = Code39(nric, add_checksum = False, writer=barcode.writer.ImageWriter())
             os.chdir(delete_path)
             image = code39.save(filename)
@@ -58,9 +86,6 @@ def generate():
             # b = delete_path
             # shutil.move(a,b)
             return render_template("barcode.html", nric=nric)
-        elif nric == '':
-            error = '<Empty Input>'
-            return render_template("index.html", error=error)
         else:
             error = '<Invalid NRIC>'
             check = True
